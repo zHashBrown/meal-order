8196e25b07c16c93b9013ddac4310d6d2847ef76
Xu Zhao <1321523567@qq.com>
2024-01-22 22:20:50 -0500

    前期准备工作

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// 设置移动端的适配
+// 除以几视口的宽度就是多少rem，现在我们设置视口的总宽度为750rem
+document.documentElement.style.fontSize = 100 / 750 + 'vw';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
